2b5a528a0cf210d16bbca09c53ce6bb59f5530c2
Stefanie Fluin <sjtrimble@gmail.com>
2017-02-24 13:47:52 -0800

    port update on server file

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var app             =   express();
 var bp              =   require('body-parser');
 var root            =   __dirname;
 var mongoose        =   require('mongoose');
+var port            =   8000;
 
 app.use(express.static(path.join(root, './client')));
 app.use(express.static(path.join(root, './bower_components')));
 app.use(bp.json());
 require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
-app.listen(8000, function() {
-    console.log("Server listening on port 8000");
+app.listen(port, function() {
+    console.log(`Server listening on port ${port}`);
 })
